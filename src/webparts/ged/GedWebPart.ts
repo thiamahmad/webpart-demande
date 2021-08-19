ff46a1cd954ac16aa60d9d122a914f22ffb27ee1
ahmet.thiam <ahmet.thiam@uvs.edu.sn>
2021-08-19 18:38:53 +0000

    maj AT

--- src/webparts/ged/GedWebPart.ts
+++ src/webparts/ged/GedWebPart.ts
@@ -29,109 +29,126 @@ export default class GedWebPart extends BaseClientSideWebPart<IGedWebPartProps> 
     <div class="container">
     <div class="card no-radius">
         <div class="card-header marge">
-            <h4><i class="fa fa-file text-dark fa-lg"></i> Acquisition documents</h4>
+            <h4><i class="fa fa-file text-dark fa-lg"></i> Demande de validation</h4>
         </div>
         <div class="card-body">
             <div class="alert alert-info" id="loading">Loading...</div>
             
-            <!--div class="form-row">
+            <div class="form-row">
+                <div class="form-group col-md-4">
+                    <label class="">Code agence</label>
+                    <input type="text" class="form-control" id="codeAgence" placeholder="BI900" />
+                </div>
+                <div class="form-group col-md-4">
+                    <label class="">Code guichet</label>
+                    <input type="text" class="form-control" id="codeGuichet" placeholder="BI900" />
+                </div>
+                <div class="form-group col-sm-4">
+                    <label>Date</label>
+                    <input type="date" class="form-control font-ms" id="date" />
+                </div>
+            </div>
+
+            <div class="form-row">
                 <div class="form-group col-md-12">
-                    <label class="">Type d'operation</label>
-                    <select class="form-control">
-                        <option value="">Retrait</option>
-                        <option value="">Versement</option>
+                    <label class="">Type d'opération</label>
+                    <select class="form-control" id="typeOperation">
+                        <option value="">Choisir le type d'opération</option>
+                        <option value="R">Retrait</option>
+                        <option value="V">Versement</option>
                     </select>
+                </div>
+            </div>
+
+            <!--div class="form-row">
+                <div class="form-group col-md-6">
+                  <label>Type d'opération</label>
+                </div>
+                <div class="form-group col-md-6">
+                  <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="type-operation" id="radioRetrait" value="retrait">
+                    <label class="form-check-label">Retrait</label>
+                  </div>
+                  <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="type-operation" id="radioVersement" value="versement">
+                    <label class="form-check-label">Versement</label>
+                  </div>
                 </div>
             </div-->
 
             <div class="form-row">
-                <div class="form-group col-md-6">
-                <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="radio" name="type-operation" id="radioRetrait" value="retrait">
-                  <label class="form-check-label">Retrait</label>
-                </div>
-                </div>
-                <div class="form-group col-md-6">
-                <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="radio" name="type-operation" id="radioVersement" value="versement">
-                  <label class="form-check-label">Versement</label>
-                </div>
-                </div>
-            </div>
-
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label class="">Code agence</label>
-                    <input type="text" class="form-control" id="codeAgence" placeholder="BI900" />
-                </div>
-                <div class="form-group col-md-6">
-                    <label class="">Code guichet</label>
-                    <input type="text" class="form-control" id="codeGuichet" placeholder="BI900" />
-                </div>
-            </div>
-
-            <div class="form-row">
                 <div class="form-group col-sm-6">
-                    <label>Date</label>
-                    <input type="date" class="form-control font-ms" id="date" />
-                </div>
-                <div class="form-group col-sm-6">
-                    <label>Numero operation</label>
+                    <label>Numero opération</label>
                     <input type="text" class="form-control" id="numeroOperation" />
                 </div>
-            </div>
-
-
-            <div class="form-row" id="blocRetrait">
-                <div class="form-group col-sm-12">
-                    <label>Cheque ?</label>
-                    <select class="form-control" name="versement">
-                        <option value="AP">Au porteur</option>
-                        <option value="TI">Titulaire</option>
-                    </select>
-                 </div>
-            </div>
-
-            <div class="form-row" id="blocVersement">
-                <div class="form-group col-sm-12">
-                    <label>Versement ?</label>
-                    <select class="form-control" name="versement">
-                        <option value="NT">Non titulaire</option>
-                        <option value="TI">Titulaire</option>
-                    </select>
-                 </div>
-            </div>
-
-            <div class="form-row">
-                <div class="form-group col-sm-12">
-                    <label>Document 1</label>
-                    <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="validatedCustomFile" required>
-                        <label class="custom-file-label" for="validatedCustomFile">Choose file...</label>
-                        <div class="invalid-feedback">Example invalid custom file feedback</div>
-                    </div>
+                <div class="form-group col-sm-6">
+                    <label>Numéro client</label>
+                    <input type="text" class="form-control" id="numeroClient" />
                 </div>
             </div>
 
             <div class="form-row">
                 <div class="form-group col-sm-12">
-                    <label>Document 2</label>
+                    <label>Fiche de vigilance</label>
                     <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="validatedCustomFile" required>
-                        <label class="custom-file-label" for="validatedCustomFile">Choose file...</label>
-                        <div class="invalid-feedback">Example invalid custom file feedback</div>
+                        <input type="file" class="custom-file-input" id="ficheVigilance" required>
+                        <label class="custom-file-label" for="ficheVigilance">Choose file...</label>
                     </div>
                 </div>
             </div>
 
             <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label class="">Montant</label>
+                    <input type="numeric" min="1000000" class="form-control" id="montant" />
+                </div>
+                <div class="form-group col-sm-6">
+                    <label>Référence du chèque</label>
+                    <input type="numeric" class="form-control" id="referenceCheque" />
+                </div>
+            </div>
+
+            <div class="form-row">
                 <div class="form-group col-sm-12">
-                    <label>Document 3</label>
-                    <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="validatedCustomFile" required>
-                        <label class="custom-file-label" for="validatedCustomFile">Choose file...</label>
-                        <div class="invalid-feedback">Example invalid custom file feedback</div>
-                    </div>
+                    <label>Bénéficiaire</label>
+                    <input type="text" class="form-control" id="beneficiaire" />
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="form-group col-sm-12">
+                    <label>Motif de la demande</label>
+                    <textarea class="form-control" id="motif" ></textarea>
+                </div>
+            </div>
+
+            <div id="blocDocument">
+                <div class="form-row">
+                  <div class="form-group col-sm-12">
+                      <label>Fiche de vigilance</label>
+                      <div class="custom-file">
+                          <input type="file" class="custom-file-input" id="ficheVigilance" required>
+                          <label class="custom-file-label" for="ficheVigilance">Choose file...</label>
+                      </div>
+                  </div>
+                </div>
+                <div class="form-row">
+                  <div class="form-group col-sm-12">
+                      <label>Piece d'identite</label>
+                      <div class="custom-file">
+                          <input type="file" class="custom-file-input" id="pieceIdentite" required>
+                          <label class="custom-file-label" for="pieceIdentite">Choose file...</label>
+                      </div>
+                  </div>
+                </div>
+                <div class="form-row">
+                  <div class="form-group col-sm-12">
+                      <label>Cheque</label>
+                      <div class="custom-file">
+                          <input type="file" class="custom-file-input" id="cheque" required>
+                          <label class="custom-file-label" for="cheque">Choose file...</label>
+                      </div>
+                  </div>
                 </div>
             </div>
 
@@ -165,6 +182,19 @@ export default class GedWebPart extends BaseClientSideWebPart<IGedWebPartProps> 
           $("#blocVersement").hide();
         }
       });
+
+      $("#typeOperation").change(() => {
+        let type = $("#typeOperation").val();
+        if (type === 'V') {
+          $("#blocRetrait").hide();
+          $("#blocVersement").show();
+        } else {
+          $("#blocRetrait").show();
+          $("#blocVersement").hide();
+        }
+      });
+
+      
 
       $("#btnReset").click(() => {
         $("#loading").empty().hide();
